231962ccf1eb71bc7f6c1520c8c4d0cf3e9d3136
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-05-29 16:02:30 +0530

    Time: 1 ms (99.89%) | Memory: 46.1 MB (28.23%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,19 +1,12 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int n=nums.length;
-    
-    for(int i=0;i<n;i++){
-        int count=0;
-        for(int j=0;j<n;j++){
-        if(nums[i]==nums[j]){
-         count++;
-        }
-        }
-        if(count==1){
-            return nums[i];
-
-        }
+    public int singleNumber(int[] arr) {
+ 
+   int xor=0;
+    for(int i=0;i<arr.length;i++){
+        xor=xor^arr[i];
     }
-    return -1;
+    return xor;
+
+
     }
 }
