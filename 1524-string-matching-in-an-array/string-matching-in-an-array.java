1c0849a09ed77a10d98b7087077045366cc0ecb9
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-07-18 22:57:55 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (8.64%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,17 +1,38 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-         List<String> ans = new ArrayList<>();
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (words[j].contains(words[i])) {
-                    ans.add(words[i]);
-                    break;
-                }
+        //  List<String> ans = new ArrayList<>();
+        // for (int i = 0; i < words.length; i++) {
+        //     for (int j = 0; j < words.length; j++) {
+        //         if (i == j) {
+        //             continue;
+        //         }
+        //         if (words[j].contains(words[i])) {
+        //             ans.add(words[i]);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+          List<String> list = new ArrayList<>();
+
+        for(int i=0;i<words.length;i++){
+            if(isSub(words,words[i],i)){
+                list.add(words[i]);
             }
         }
-        return ans;
+        return list;
+
+    }
+
+    private Boolean isSub(String[] words,String word,int curr){
+
+        for(int i=0;i<words.length;i++){
+            if(i!=curr && words[i].contains(word)){
+                return true;   
+            }
+        }
+
+        return false;
     }
 }
