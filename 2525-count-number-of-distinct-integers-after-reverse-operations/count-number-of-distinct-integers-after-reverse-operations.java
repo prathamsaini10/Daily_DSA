2c7490c96c8603791a7a092c43adb5f653c5e679
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-07-24 01:19:28 +0530

    Time: 65 ms (45.22%) | Memory: 65.9 MB (27.13%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,14 +1,30 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-    Set<Integer> set=new HashSet<>();
+    // Set<Integer> set=new HashSet<>();
 
-        for(int i=0;i<nums.length;i++){
-         set.add(nums[i]);
-         String str=Integer.toString(nums[i]);
-         StringBuilder str1=new StringBuilder(str);
-         str1.reverse();
-         set.add(Integer.parseInt(str1.toString()));
+    //     for(int i=0;i<nums.length;i++){
+    //      set.add(nums[i]);
+    //      String str=Integer.toString(nums[i]);
+    //      StringBuilder str1=new StringBuilder(str);
+    //      str1.reverse();
+    //      set.add(Integer.parseInt(str1.toString()));
+    //     }
+    //     return set.size();
+
+    HashSet<Integer> s = new HashSet<>();
+        for(int i = 0 ; i<nums.length ; i++){
+            s.add(nums[i]);
+            int rev = revNumber(nums[i]);
+            s.add(rev);
         }
-        return set.size();
+        return s.size();
+    }
+    public int revNumber(int num){
+        int rev = 0;
+        while(num != 0){
+            rev = rev * 10 + num%10;
+            num = num/10;
+        }
+        return rev;
     }
 }
