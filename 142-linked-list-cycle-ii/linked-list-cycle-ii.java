f8be6f71f29ef3e5bb4192702ea778877c9539ec
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-07-27 01:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (28.92%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,17 +9,57 @@
  *     }
  * }
  */
+
+
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-      HashMap<ListNode,Integer> map=new HashMap<>();
-      ListNode temp=head;
-      while(temp!=null){
-        if(map.containsKey(temp)){
-            return temp;
+     public ListNode detectCycle(ListNode head) {
+//       HashMap<ListNode,Integer> map=new HashMap<>();
+//       ListNode temp=head;
+//       while(temp!=null){
+//         if(map.containsKey(temp)){
+//             return temp;
+//         }
+//         map.put(temp,1);
+//     temp=temp.next;
+//       }
+//       return null;
+
+        //TORTOISE HARE
+  ListNode slow = head;  
+        ListNode fast = head;  
+    
+        // Phase 1: Detect the loop
+        while (fast != null && fast.next != null) {
+            // Move slow one step
+            slow = slow.next;        
+            
+            // Move fast two steps
+            fast = fast.next.next;  
+    
+            // If slow and fast meet,
+            // a loop is detected
+            if (slow == fast) {
+                // Reset the slow pointer
+                // to the head of the list
+                slow = head; 
+    
+                // Phase 2: Find the first node of the loop
+                while (slow != fast) {
+                    // Move slow and fast one step
+                    // at a time
+                    slow = slow.next;  
+                    fast = fast.next;  
+    
+                    // When slow and fast meet again,
+                    // it's the first node of the loop
+                }
+                
+                // Return the first node of the loop
+                return slow;  
+            }
         }
-        map.put(temp,1);
-    temp=temp.next;
-      }
-      return null;
+        
+        // If no loop is found, return null
+        return null; 
     }
 }
