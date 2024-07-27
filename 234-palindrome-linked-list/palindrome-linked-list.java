9981b83c5e90ab8d8e5693165caba9753483699e
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-07-27 10:39:47 +0530

    Time: 12 ms (20.50%) | Memory: 72.6 MB (5.38%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,21 +9,56 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head){
+        if(head==null||head.next==null)return head;
+
+        ListNode newhead=reverse(head.next);
+        ListNode front =head.next;
+        front.next=head;
+        head.next=null;
+return newhead;
+    }
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while (temp != null) {
-            st.push(temp.val);
-            temp = temp.next;
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while (temp != null) {
+        //     if (temp.val != st.peek()) {
+        //         return false;
+        //     }
+        //     st.pop();
+        //     temp = temp.next;
+        // }
+        // return true;
+
+                //TORTOISE HARE
+                   if(head == null || head.next==null )return true;
+                
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            
         }
-        temp = head;
-        while (temp != null) {
-            if (temp.val != st.peek()) {
+        ListNode newhead =reverse(slow.next);
+
+        ListNode last=newhead;
+        ListNode first = head;
+        while(last!=null){
+            if(first.val!=last.val){
+                reverse(newhead);
                 return false;
             }
-            st.pop();
-            temp = temp.next;
+            first=first.next;
+            last=last.next ;
+            
         }
+        reverse(newhead);
         return true;
     }
 }
