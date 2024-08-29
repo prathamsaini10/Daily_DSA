d0b926c207cd0098596d02e07ec43ff7c11860ad
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-08-29 16:36:17 +0530

    Time: 13 ms (28.78%) | Memory: 49.7 MB (73.81%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,26 +1,38 @@
 class Solution {
     public int majorityElement(int[] arr) {
         int n=arr.length;
-     int count=0;
-     int ele=0;
-     for(int i=0;i<n;i++){
-        if(count==0){
-            count=1;
-            ele=arr[i];
-        }else if(ele==arr[i]){
-            count++;
-        }else count--;
-     }
-     int count1=0;
-     for(int i=0;i<n;i++){
-        if(arr[i]==ele){
-            count1++;
-        }
+    //  int count=0;
+    //  int ele=0;
+    //  for(int i=0;i<n;i++){
+    //     if(count==0){
+    //         count=1;
+    //         ele=arr[i];
+    //     }else if(ele==arr[i]){
+    //         count++;
+    //     }else count--;
+    //  }
+    //  int count1=0;
+    //  for(int i=0;i<n;i++){
+    //     if(arr[i]==ele){
+    //         count1++;
+    //     }
      
-     }
-            if(count1>(n/2)){
-                return ele;
-            }
-            return -1;
+    //  }
+    //         if(count1>(n/2)){
+    //             return ele;
+    //         }
+    //         return -1;
+
+    HashMap<Integer,Integer> map=new HashMap<>();
+    for(int i=0;i<n;i++){
+        map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+    }
+    for(Map.Entry<Integer,Integer> it:map.entrySet()){
+        if(it.getValue()>(n/2)){
+            return it.getKey();
+        }
+    
+    }
+        return -1;
     }
 }
