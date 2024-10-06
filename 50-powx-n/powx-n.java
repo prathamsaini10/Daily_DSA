7acf10ceae76720411e269d17c4afaedeea3c2ad
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-10-06 06:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (24.61%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,48 @@
 class Solution {
-    public double myPow(double x, int n) {
-          if(n < 0){
-            n = -n;
-            x = 1 / x;
+    public double myPow(double x, int n1) {
+        // return solve(x,(long)n);
+    double ans=1;
+    long n=n1;
+    if(n>=0){
+        while(n>0){
+        if((n&1)==0){
+            n=n/2;
+            x*=x;
+        }else {
+            ans*=x;
+            n=n-1;
         }
-        
-        double pow = 1;
-        
-        while(n != 0){
-            if((n & 1) != 0){
-                pow *= x;
-            } 
-                
-            x *= x;
-            n >>>= 1;
-            
+     
+    }
+    }else{
+        x=1/x;
+        n=n*(-1);
+          while(n>0){
+        if((n&1)==0){
+            n=n/2;
+            x*=x;
+        }else {
+            ans*=x;
+            n=n-1;
         }
-        
-        return pow;
+     
+    }
+    }
+       return ans;
+    }
+
+    public double solve (double x,long n){
+
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return solve(1/x,-n);
+        }
+        if((n&1)==0){
+            return solve(x*x,n/2);
+        }
+        return x*solve(x*x,(n-1)/2);
+
     }
 }
