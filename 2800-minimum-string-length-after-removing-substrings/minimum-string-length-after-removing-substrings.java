2d5e2ae57ac55d22199ec1b18304d25232a1210e
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-10-07 07:16:47 +0530

    Time: 8 ms (25.28%) | Memory: 44.7 MB (21.19%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,20 +1,34 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(st.isEmpty()){
-                    st.push(ch);
-                    continue;
-            }
-            if(ch=='B'&&st.peek()=='A'){
-                st.pop();
-            }else if(ch=='D'&&st.peek()=='C'){
-                st.pop();
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(st.isEmpty()){
+        //             st.push(ch);
+        //             continue;
+        //     }
+        //     if(ch=='B'&&st.peek()=='A'){
+        //         st.pop();
+        //     }else if(ch=='D'&&st.peek()=='C'){
+        //         st.pop();
+        //     }else{
+        //         st.push(ch);
+        //     }
+        // }
+        // return st.size();
+
+
+        boolean check=true;
+        while(check!=false){
+            if(s.contains("AB")){
+                s=s.replace("AB","");
+            }else if(s.contains("CD")){
+                s=s.replace("CD","");
             }else{
-                st.push(ch);
+                check=false;
+
             }
         }
-        return st.size();
+        return s.length();
     }
 }
