05988de9c7b8578b874cc9eeb78d44c61d4ccc92
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-10-11 08:25:27 +0530

    Time: 8 ms (18.11%) | Memory: 44.2 MB (63.59%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -16,50 +16,50 @@ class Solution {
        map.put(1000,"M");
         StringBuilder st=new StringBuilder();
         int nums=num;
-       while(nums>0){
-        // if(num>=1000){
-        //     st.append(map.get(1000));
-        //     num-=1000;
-        // }else if(num>=900){
-        //      st.append(map.get(900));
-        //     num-=900;
-        // }else if(num>=500){
-        //      st.append(map.get(500));
-        //     num-=500;
-        // }else if(num>=400){
-        //      st.append(map.get(400));
-        //     num-=400;
-        // }else if(num>=100){
-        //      st.append(map.get(100));
-        //     num-=100;
-        // }else if(num>=90){
-        //      st.append(map.get(90));
-        //     num-=90;
-        // }else if(num>=50){
-        //  st.append(map.get(50));
-        //     num-=50;
-        // }else if(num>=40){
-        //      st.append(map.get(40));
-        //     num-=40;
-        // }else if(num>=10){
-        //      st.append(map.get(10));
-        //     num-=10;
-        // }else if(num>=9){
-        //      st.append(map.get(9));
-        //     num-=9;
-        // }else if(num>=5){
-        //      st.append(map.get(5));
-        //     num-=5;
-        // }else if(num>=4){
-        //      st.append(map.get(4));
-        //     num-=4;
-        // }else{
-        //      st.append(map.get(1));
-        //     num-=1;
-        // }
-      Integer closeKey = map.floorKey(nums);
-     st.append(map.get(closeKey));
-     nums-=closeKey;
+       while(num>0){
+        if(num>=1000){
+            st.append(map.get(1000));
+            num-=1000;
+        }else if(num>=900){
+             st.append(map.get(900));
+            num-=900;
+        }else if(num>=500){
+             st.append(map.get(500));
+            num-=500;
+        }else if(num>=400){
+             st.append(map.get(400));
+            num-=400;
+        }else if(num>=100){
+             st.append(map.get(100));
+            num-=100;
+        }else if(num>=90){
+             st.append(map.get(90));
+            num-=90;
+        }else if(num>=50){
+         st.append(map.get(50));
+            num-=50;
+        }else if(num>=40){
+             st.append(map.get(40));
+            num-=40;
+        }else if(num>=10){
+             st.append(map.get(10));
+            num-=10;
+        }else if(num>=9){
+             st.append(map.get(9));
+            num-=9;
+        }else if(num>=5){
+             st.append(map.get(5));
+            num-=5;
+        }else if(num>=4){
+             st.append(map.get(4));
+            num-=4;
+        }else{
+             st.append(map.get(1));
+            num-=1;
+        }
+    //   Integer closeKey = map.floorKey(nums);
+    //  st.append(map.get(closeKey));
+    //  nums-=closeKey;
 
        }
 return st.toString();
