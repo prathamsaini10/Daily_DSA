1ed4d46f80ab1435f616bf42d3835685e87d0255
prathamsaini10 <131442979+prathamsaini10@users.noreply.github.com>
2024-11-10 22:17:08 +0530

    Time: 9 ms (51.74%) | Memory: 46.4 MB (71.17%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -11,23 +11,15 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
 
         for (int i = 0; i < n; i++) { // select an interval:
-            int start = arr[i][0];
-            int end = arr[i][1];
-
-            //Skip all the merged intervals:
-            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
-                continue;
+             if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(arr[i][0], arr[i][1]));
             }
-
-            //check the rest of the intervals:
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][0] <= end) {
-                    end = Math.max(end, arr[j][1]);
-                } else {
-                    break;
-                }
+            // if the current interval
+            // lies in the last interval:
+            else {
+                ans.get(ans.size() - 1).set(1,
+                                            Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
             }
-            ans.add(Arrays.asList(start, end));
         }
      int rows = ans.size();
         int columns = ans.get(0).size(); 
